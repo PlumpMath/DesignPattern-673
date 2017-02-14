d5aa7347343d145feb83b6a4d93b9988a272bbf1
wangxingyong <wangxingyong@pset.suntec.net>
2017-02-14 18:10:57 +0800

    add Mode

--- DesignPattern/src/Facade/Presentation.java
+++ DesignPattern/src/Facade/Presentation.java
@@ -6,11 +6,14 @@ package Facade;
  * 下午5:50:44
  * 2017
  */
-public class GenPresentation {
+public class Presentation {
 	/*
 	 * 生成表现层（generate生成）
 	 */
 	public void generate() {
 		ConfigMode cm=ConfigManger.getInstance().getConfigMode();
+		if(cm.isNeedGenPresentation()){
+			System.out.println("生成表现层代码文件");
+		}
 	}
 }
